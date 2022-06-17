043e1ce63c7ee74c85696198fd85f7b1a969de1d
Kumar Aman <kaman0353@gmail.com>
2022-06-17 16:22:06 +0530

    Fix: Project content loader

--- src/component/projects/Projects.jsx
+++ src/component/projects/Projects.jsx
@@ -4,22 +4,22 @@ import ProjectsCard from "../project-card/ProjectCard";
 function Projects() {
   const [projectList, setProjectList] = useState([]);
   useEffect(() => {
-    if ("https://storage.googleapis.com/kaman-main/static-files/portfolioapp/projectList.json") {
-      fetch("https://storage.googleapis.com/kaman-main/static-files/portfolioapp/projectList.json", 
-      {method: 'GET', 
-      mode: 'no-cors'})
-        .then((resp) => resp.json())
-        .then((list) => setProjectList([...projectList, list]));
-    }
-  });
+    let url = process.env.REACT_APP_PROJECT_URL;
+    fetch(url, {
+      mode: "no-cors",
+    })
+      .then((resp) => resp.text())
+      .then((list) => setProjectList([list]));
+  }, []);
 
   return (
     <div className="project-wrapper center-conatiner">
       <div className="heading-main">Projects</div>
       <main className="projects flex">
         {projectList.map(
-          ({ projectImage, projectTitle, sourceCodeLink, previewLink }) => (
+          ({ projectImage, projectTitle, sourceCodeLink, previewLink }, id) => (
             <ProjectsCard
+              key={id}
               projectImage={projectImage}
               projectTitle={projectTitle}
               sourceCodeLink={sourceCodeLink}
