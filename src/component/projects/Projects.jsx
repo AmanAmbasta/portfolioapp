456faa82cc25202fcef99304ec4d60fb2345f0e8
AmanAmbasta <kaman0353@gmail.com>
2022-07-03 21:08:29 +0530

    Fix: Projects list api call

--- src/component/projects/Projects.jsx
+++ src/component/projects/Projects.jsx
@@ -1,31 +1,23 @@
-import { useEffect, useState } from "react";
 import "./project.css";
 import ProjectsCard from "../project-card/ProjectCard";
+import proj from "../../projectList.json"
 function Projects() {
-  const [projectList, setProjectList] = useState([]);
-  useEffect(() => {
-    let url = process.env.REACT_APP_PROJECT_URL+"?a="+(Math.floor(Math.random()*9999));
-    fetch(url, {
-      mode: "no-cors",
-    })
-      .then((resp) => resp.text())
-      .then((list) => setProjectList([list]));
-  }, []);
-
-  return (
+   return (
     <div className="project-wrapper center-conatiner">
       <div className="heading-main">Projects</div>
       <main className="projects flex">
-        {projectList.map(
-          ({ projectImage, projectTitle, sourceCodeLink, previewLink }, id) => (
-            <ProjectsCard
-              key={id}
-              projectImage={projectImage}
-              projectTitle={projectTitle}
-              sourceCodeLink={sourceCodeLink}
-              previewLink={previewLink}
-            />
-          )
+        {proj.map(
+          ({ projectImage, projectTitle, sourceCodeLink, previewLink }, id) => {
+            return (
+              <ProjectsCard
+                key={id}
+                projectImage={projectImage}
+                projectTitle={projectTitle}
+                sourceCodeLink={sourceCodeLink}
+                previewLink={previewLink}
+              />
+            );
+          }
         )}
       </main>
     </div>
